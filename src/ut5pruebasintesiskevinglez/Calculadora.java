6e5dea00ebcafc4dc1d38c829a959a313e7651e2
Nelu <Nelu>
2016-04-08 15:55:42 +0100

    Paso 3 y 4

--- src/ut5pruebasintesiskevinglez/Calculadora.java
+++ src/ut5pruebasintesiskevinglez/Calculadora.java
@@ -18,5 +18,47 @@ public class Calculadora {
     private static void main(String[] args) {
 
     }
+
+    /**
+     * @return the operador1
+     */
+    public int getOperador1() {
+        return operador1;
+    }
+
+    /**
+     * @param operador1 the operador1 to set
+     */
+    public void setOperador1(int operador1) {
+        this.operador1 = operador1;
+    }
+
+    /**
+     * @return the operador2
+     */
+    public int getOperador2() {
+        return operador2;
+    }
+
+    /**
+     * @param operador2 the operador2 to set
+     */
+    public void setOperador2(int operador2) {
+        this.operador2 = operador2;
+    }
+
+    /**
+     * @return the operacion
+     */
+    public int getOperacion() {
+        return operacion;
+    }
+
+    /**
+     * @param operacion the operacion to set
+     */
+    public void setOperacion(int operacion) {
+        this.operacion = operacion;
+    }
     
 }
