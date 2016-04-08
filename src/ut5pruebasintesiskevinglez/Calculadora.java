564c90c96cecb770e42d08f2e5efe26670cac494
Nelu <Nelu>
2016-04-08 15:52:57 +0100

    Paso 1 y 2

---
+++ src/ut5pruebasintesiskevinglez/Calculadora.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ut5pruebasintesiskevinglez;
+
+/**
+ *
+ * @author KevinGlez
+ */
+public class Calculadora {
+
+    private int operador1;
+    private int operador2;
+    private int operacion;
+       
+    private static void main(String[] args) {
+
+    }
+    
+}
